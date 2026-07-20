3b61770bbbb3c2b14d6f443b575bd3863abb73c9
mksk26066-pixel <mksk26066@gmail.com>
2026-07-20 16:05:36 +0530

    Fix debug block syntax in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,7 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug
-  }
+    debug {
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
